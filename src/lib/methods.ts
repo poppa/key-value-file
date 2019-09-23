946831a3f2acd011f7ef3a417a03d6e0d0c9c0e6
Pontus Östlund <ponost@roxen.com>
2019-09-23 08:12:20 +0200

    Check explicitly for file existence in `parseFile()`

--- src/lib/methods.ts
+++ src/lib/methods.ts
@@ -2,8 +2,13 @@ import { PathLike } from "fs"
 import { tokenize } from './tokenize'
 import { KeyValue } from './keyvalue'
 import { KeyValueFile } from './keyvaluefile'
+import { exists } from './fs'
 
 export async function parseFile(path: PathLike) {
+  if (!await exists(path)) {
+    throw new Error(`ENOENT, no such file or directory '${path}'`)
+  }
+
   return KeyValueFile.create(path)
 }
 
