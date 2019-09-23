2f2b32c1ac132cf894d67a09e313f05f87b19070
Pontus Östlund <ponost@roxen.com>
2019-09-23 08:05:11 +0200

    Use `KeyValueFile.create()` when loading a file

--- src/lib/methods.ts
+++ src/lib/methods.ts
@@ -1,12 +1,10 @@
 import { PathLike } from "fs"
-import { readFile } from './fs'
 import { tokenize } from './tokenize'
 import { KeyValue } from './keyvalue'
 import { KeyValueFile } from './keyvaluefile'
 
 export async function parseFile(path: PathLike) {
-  const data = await readFile(path)
-  return new KeyValueFile(path, tokenize(data))
+  return KeyValueFile.create(path)
 }
 
 export function parseString(data: string | Buffer) {
