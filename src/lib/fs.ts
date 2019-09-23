bb97cca779b6399df142315ee016d66a4105220f
Pontus Östlund <ponost@roxen.com>
2019-09-23 08:04:50 +0200

    `exists()` checks the exitense of a file

--- src/lib/fs.ts
+++ src/lib/fs.ts
@@ -1,5 +1,16 @@
-import fs from 'fs'
+import fs, { PathLike } from 'fs'
 import { promisify } from "util"
+
+const stat = promisify(fs.stat)
 
 export const readFile = promisify(fs.readFile)
 export const writeFile = promisify(fs.writeFile)
+export const exists = async (path: PathLike) => {
+  try {
+    const s = await stat(path)
+    return !!s
+  }
+  catch (e) {
+    return false
+  }
+}
